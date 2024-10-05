7e44a24693f88d2650432f74d05d422b836e607e
miznaansari <71661652+miznaansari@users.noreply.github.com>
2024-10-06 03:19:08 +0530

    Add confirmation model before delete

--- src/components/Username.js
+++ src/components/Username.js
@@ -1,22 +1,30 @@
 import React from 'react'
 
 const Username = (props) => {
-    const { item, handleupdate ,handledelete } =  props;
-  return (
-    <tr key={item.id}>
-    <th scope="row">{item.id}</th>
-    <td>{item.name}</td>
-    <td>{item.username}</td>
-    <td>{item.email}</td>
-    <td>
-        {item.address.street}, {item.address.suite}, {item.address.city}, {item.address.zipcode}
-    </td>
-    <td><button onClick={() => handledelete(item.id)} className='btn btn-danger'>Delete</button></td>
-    <td><button type="button" onClick={() => handleupdate(item )} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Update
-    </button></td>
-</tr>
-  )
+    const { item, handleupdate, handledelete } = props;
+    return (
+        <tr key={item.id}>
+            <th scope="row">{item.id}</th>
+            <td>{item.name}</td>
+            <td>{item.username}</td>
+            <td>{item.email}</td>
+            <td>
+                {item.address.street}, {item.address.suite}, {item.address.city}, {item.address.zipcode}
+            </td>
+            <td><button onClick={() => {
+                    if (window.confirm('Are you sure you want to delete this user?')) {
+                        handledelete(item.id);
+                    }
+                }}
+                className='btn btn-danger'
+            >
+                Delete
+            </button></td>
+            <td><button type="button" onClick={() => handleupdate(item)} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Update
+            </button></td>
+        </tr>
+    )
 }
 
 export default Username
